2635fcc1ac437bf680e9564bf1b8360732882fc8
SeanMa2018 <sean.ma@thinkpower-info.com>
2020-10-17 00:22:34 +0800

    update

--- BlazorServer/BlazorServer/Startup.cs
+++ BlazorServer/BlazorServer/Startup.cs
@@ -43,10 +43,14 @@ namespace BlazorServer
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
+                //啟動強制使用https通訊
                 app.UseHsts();
             }
 
+            //啟動自動跳轉為https
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -55,6 +59,8 @@ namespace BlazorServer
             {
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
+
+                //使用api
                 endpoints.MapControllers();
             });
         }
