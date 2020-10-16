c996ea0afd9a4bbccc161d4dbc6407b61e0247d3
SeanMa2018 <sean.ma@thinkpower-info.com>
2020-10-17 00:20:23 +0800

    新增轉譯說明，新增API範例

---
+++ BlazorServer/BlazorServer/ReadMe.md
@@ -0,0 +1,10 @@
+﻿# 導覽
+
+## 如何安裝使用
+
+### 檔案轉換
+
+.\openssl.exe pkcs12 -export -out C:\Users\purem\Downloads\thetainformation.com\certificate.pfx 
+-inkey C:\Users\purem\Downloads\thetainformation.com\private.key 
+-in C:\Users\purem\Downloads\thetainformation.com\certificate.crt 
+-certfile C:\Users\purem\Downloads\thetainformation.com\ca_bundle.crt
--- BlazorServer/BlazorServer/Startup.cs
+++ BlazorServer/BlazorServer/Startup.cs
@@ -29,6 +29,7 @@ namespace BlazorServer
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,10 +43,10 @@ namespace BlazorServer
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -54,6 +55,7 @@ namespace BlazorServer
             {
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapControllers();
             });
         }
     }
