fceb97d737d15023b26c2b39480bb6b7558c7be0
SeanMa2018 <sean.ma@thinkpower-info.com>
2021-01-10 00:19:31 +0800

    更新

---
+++ EA/gimmy_pionex_v1.cpp
@@ -0,0 +1,602 @@
+//+------------------------------------------------------------------+
+//|                                              gimmy_pionex_v1.mq4 |
+//|                        Copyright 2020, MetaQuotes Software Corp. |
+//|                                             https://www.mql5.com |
+//+------------------------------------------------------------------+
+#property copyright "Copyright 2020, MetaQuotes Software Corp."
+#property link "https://www.mql5.com"
+#property version "1.00"
+#property strict
+
+#define MAGIC_NUMBER 294381
+#define TRADE_PAIR "XAUUSD"
+
+// 賣價(Ask)：金融機構賣給投資人的價格 <<投資人要買債券看Ask
+// 買價(Bid)：金融機構跟投資人買的價格 <<投資人要賣債券看Bid
+
+double Bid紀錄 = 0;
+double Ask紀錄 = 0;
+
+double 最低做多價 = 0;
+double 最高做空價 = 0;
+
+double 買單數量 = 0;
+double 賣單數量 = 0;
+
+double 讀取多單數量()
+{
+    return 買單數量;
+}
+
+double 讀取空單數量()
+{
+    return 賣單數量;
+}
+
+void 加入買單數量(double 數量)
+{
+    買單數量 += 數量;
+}
+
+void 加入賣單數量(double 數量)
+{
+    賣單數量 += 數量;
+}
+
+void 紀錄多空價格()
+{
+    Bid紀錄 = Bid;
+    Ask紀錄 = Ask;
+}
+
+void 平獲利多單()
+{
+    for (int i = 0; i < OrdersTotal(); i++)
+    {
+        if (OrderSelect(i, SELECT_BY_POS))
+        {
+            if (OrderType() == OP_BUY && Bid > OrderOpenPrice())
+            {
+                double lots = OrderLots();
+                double price = Bid;
+                bool isClosed = OrderClose(OrderTicket(), lots, price, 0, clrNONE);
+
+                if (isClosed)
+                {
+                    加入買單數量(-lots);
+                }
+            }
+        }
+    }
+}
+
+void 平獲利空單()
+{
+    for (int i = 0; i < OrdersTotal(); i++)
+    {
+        if (OrderSelect(i, SELECT_BY_POS))
+        {
+            if (OrderType() == OP_SELL && OrderOpenPrice() > Ask)
+            {
+                double lots = OrderLots();
+                double price = Ask;
+                bool isClosed = OrderClose(OrderTicket(), lots, price, 0, clrNONE);
+
+                if (isClosed)
+                {
+                    加入賣單數量(-lots);
+                }
+            }
+        }
+    }
+}
+
+bool 所有多單虧損()
+{
+    for (int i = 0; i < OrdersTotal(); i++)
+    {
+        if (OrderSelect(i, SELECT_BY_POS))
+        {
+            if (OrderType() == OP_BUY && Bid > OrderOpenPrice())
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool 所有空單虧損()
+{
+    for (int i = 0; i < OrdersTotal(); i++)
+    {
+        if (OrderSelect(i, SELECT_BY_POS))
+        {
+            if (OrderType() == OP_SELL && OrderOpenPrice() > Ask)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool 有獲利多單()
+{
+    for (int i = 0; i < OrdersTotal(); i++)
+    {
+        if (OrderSelect(i, SELECT_BY_POS))
+        {
+            if (OrderType() == OP_BUY && Bid > OrderOpenPrice())
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool 有獲利空單()
+{
+    for (int i = 0; i < OrdersTotal(); i++)
+    {
+        if (OrderSelect(i, SELECT_BY_POS))
+        {
+            if (OrderType() == OP_SELL && OrderOpenPrice() > Ask)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+void 平損失最多的空單直到空單剩下九張()
+{
+}
+
+double 計算本次空單獲利()
+{
+    return 0;
+}
+
+double 最遠多單損失()
+{
+    return 0;
+}
+
+double 第二遠多單損失()
+{
+    return 0;
+}
+
+//+------------------------------------------------------------------+
+//| Expert initialization function                                   |
+//+------------------------------------------------------------------+
+int OnInit()
+{
+    //---
+
+    //---
+    return (INIT_SUCCEEDED);
+}
+//+------------------------------------------------------------------+
+//| Expert deinitialization function                                 |
+//+------------------------------------------------------------------+
+void OnDeinit(const int reason)
+{
+    //---
+}
+
+//https://www.youtube.com/watch?v=tva-XMSbsLc&vl=en
+//+------------------------------------------------------------------+
+//| Expert tick function                                             |
+//+------------------------------------------------------------------+
+void OnTick()
+{
+    //---
+    if (讀取多單數量() == 0 && 讀取空單數量() == 0)
+    {
+        OrderSend(TRADE_PAIR, OP_BUY, 0.01, Ask, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+        OrderSend(TRADE_PAIR, OP_SELL, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+
+        加入買單數量(0.01);
+        加入賣單數量(0.01);
+
+        紀錄多空價格();
+    }
+
+    if (讀取多單數量() < 0.09 && 讀取空單數量() < 0.09)
+    {
+        if ((Bid - Ask紀錄) > 6)
+        {
+            平獲利多單();
+            OrderSend(TRADE_PAIR, OP_SELL, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+            加入賣單數量(0.01);
+            紀錄多空價格();
+        }
+
+        if ((Bid紀錄 - Ask) > 6)
+        {
+            平獲利空單();
+            OrderSend(TRADE_PAIR, OP_BUY, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+            加入買單數量(0.01);
+            紀錄多空價格();
+        }
+    }
+
+    if (讀取多單數量() == 0.09 && 所有多單虧損() && 讀取空單數量() < 0.18)
+    {
+        if (Bid - Ask紀錄 > 6)
+        {
+            bool 是否有獲利多單 = 有獲利多單();
+
+            if (是否有獲利多單)
+            {
+                平獲利多單();
+                if (讀取空單數量() < 0.09)
+                {
+                    OrderSend(TRADE_PAIR, OP_SELL, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                    加入賣單數量(0.01);
+                    紀錄多空價格();
+                }
+                else if (讀取空單數量() > 0.09)
+                {
+                    平損失最多的空單直到空單剩下九張();
+                    紀錄多空價格();
+                }
+            }
+
+            bool 是否有獲利空單 = 有獲利空單();
+
+            if (是否有獲利空單)
+            {
+                平獲利空單();
+                紀錄多空價格();
+
+                double 獲利 = 計算本次空單獲利();
+
+                bool 有平多單 = false;
+
+                if (獲利 > 最遠多單損失())
+                {
+                    double 損失 = 平最遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平多單 = true;
+                }
+
+                if (獲利 > 第二遠多單損失())
+                {
+                    double 損失 = 平第二遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平多單 = true;
+                }
+
+                if (獲利 > 第三遠多單損失())
+                {
+                    double 損失 = 平第三遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平多單 = true;
+                }
+
+                if (有平多單)
+                {
+                    OrderSend(TRADE_PAIR, OP_BUY, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                    加入買單數量(0.01);
+                }
+            }
+
+            if (!是否有獲利多單 && !是否有獲利空單)
+            {
+                紀錄多空價格();
+            }
+        }
+
+        if (Bid紀錄 - Ask > 6)
+        {
+            紀錄多空價格();
+
+            if (所有空單價格大於賣價紀錄(6))
+            {
+                OrderSend(TRADE_PAIR, OP_SELL, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                加入賣單數量(0.01);
+            }
+        }
+    }
+
+    if (讀取多單數量() == 0.09 && 所有多單虧損() && 讀取空單數量() == 0.18)
+    {
+        if (Bid - Ask紀錄 > 6)
+        {
+            bool 是否有獲利多單 = 有獲利多單();
+
+            if (是否有獲利多單)
+            {
+                平獲利多單();
+                if (讀取空單數量() < 0.09)
+                {
+                    OrderSend(TRADE_PAIR, OP_SELL, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                    加入賣單數量(0.01);
+                    紀錄多空價格();
+                }
+                else if (讀取空單數量() > 0.09)
+                {
+                    平損失最多的空單直到空單剩下九張();
+                    紀錄多空價格();
+                }
+            }
+
+            bool 是否有獲利空單 = 有獲利空單();
+
+            if (是否有獲利空單)
+            {
+                平獲利空單();
+                紀錄多空價格();
+
+                double 獲利 = 計算本次空單獲利();
+
+                bool 有平多單 = false;
+
+                if (獲利 > 最遠多單損失())
+                {
+                    double 損失 = 平最遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平多單 = true;
+                }
+
+                if (獲利 > 第二遠多單損失())
+                {
+                    double 損失 = 平第二遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平多單 = true;
+                }
+
+                if (獲利 > 第三遠多單損失())
+                {
+                    double 損失 = 平第三遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平多單 = true;
+                }
+
+                if (有平多單)
+                {
+                    OrderSend(TRADE_PAIR, OP_BUY, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                    加入買單數量(0.01);
+                }
+            }
+
+            if (!是否有獲利多單 && !是否有獲利空單)
+            {
+                紀錄多空價格();
+            }
+        }
+
+        if (Bid紀錄 - Ask > 6)
+        {
+            平獲利空單();
+            紀錄多空價格();
+
+            double 獲利 = 計算本次空單獲利();
+
+            bool 有平多單 = false;
+
+            if (獲利 > 最遠多單損失())
+            {
+                double 損失 = 平最遠多單();
+                獲利 = 獲利 - 損失;
+                有平多單 = true;
+            }
+
+            if (獲利 > 第二遠多單損失())
+            {
+                double 損失 = 平第二遠多單();
+                獲利 = 獲利 - 損失;
+                有平多單 = true;
+            }
+
+            if (獲利 > 第三遠多單損失())
+            {
+                double 損失 = 平第三遠多單();
+                獲利 = 獲利 - 損失;
+                有平多單 = true;
+            }
+
+            if (有平多單)
+            {
+                OrderSend(TRADE_PAIR, OP_BUY, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                加入買單數量(0.01);
+            }
+        }
+    }
+
+    //反向做
+
+    if (讀取空單數量() == 0.09 && 所有空單虧損() && 讀取多單數量() < 0.18)
+    {
+        if (Ask - Bid紀錄 > 6)
+        {
+            bool 是否有獲利空單 = 有獲利空單();
+
+            if (是否有獲利空單)
+            {
+                平獲利空單();
+                if (讀取多單數量() < 0.09)
+                {
+                    OrderSend(TRADE_PAIR, OP_BUY, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                    加入買單數量(0.01);
+                    紀錄多空價格();
+                }
+                else if (讀取多單數量() > 0.09)
+                {
+                    平損失最多的多單直到多單剩下九張();
+                    紀錄多空價格();
+                }
+            }
+
+            bool 是否有獲利多單 = 有獲利多單();
+
+            if (是否有獲利多單)
+            {
+                平獲利多單();
+                紀錄多空價格();
+
+                double 獲利 = 計算本次多單獲利();
+
+                bool 有平空單 = false;
+
+                if (獲利 > 最遠空單損失())
+                {
+                    double 損失 = 平最遠空單();
+                    獲利 = 獲利 - 損失;
+                    有平空單 = true;
+                }
+
+                if (獲利 > 第二遠空單損失())
+                {
+                    double 損失 = 平第二遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平空單 = true;
+                }
+
+                if (獲利 > 第三遠空單損失())
+                {
+                    double 損失 = 平第三遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平空單 = true;
+                }
+
+                if (有平空單)
+                {
+                    OrderSend(TRADE_PAIR, OP_SELL, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                    加入賣單數量(0.01);
+                }
+            }
+
+            if (!是否有獲利多單 && !是否有獲利空單)
+            {
+                紀錄多空價格();
+            }
+        }
+
+        if (Ask紀錄 - Bid > 6)
+        {
+            紀錄多空價格();
+
+            if (所有多單價格小於買價紀錄(6))
+            {
+                OrderSend(TRADE_PAIR, OP_BUY, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                加入買單數量(0.01);
+            }
+        }
+    }
+
+    if (讀取空單數量() == 0.09 && 所有空單虧損() && 讀取多單數量() == 0.18)
+    {
+       if (Bid紀錄 - Ask > 6)
+        {
+            bool 是否有獲利空單 = 有獲利空單();
+
+            if (是否有獲利空單)
+            {
+                平獲利空單();
+                if (讀取多單數量() < 0.09)
+                {
+                    OrderSend(TRADE_PAIR, OP_BUY, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                    加入買單數量(0.01);
+                    紀錄多空價格();
+                }
+                else if (讀取多單數量() > 0.09)
+                {
+                    平損失最多的多單直到多單剩下九張();
+                    紀錄多空價格();
+                }
+            }
+
+            bool 是否有獲利多單 = 有獲利多單();
+
+            if (是否有獲利多單)
+            {
+                平獲利多單();
+                紀錄多空價格();
+
+                double 獲利 = 計算本次多單獲利();
+
+                bool 有平空單 = false;
+
+                if (獲利 > 最遠空單損失())
+                {
+                    double 損失 = 平最遠空單();
+                    獲利 = 獲利 - 損失;
+                    有平空單 = true;
+                }
+
+                if (獲利 > 第二遠空單損失())
+                {
+                    double 損失 = 平第二遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平空單 = true;
+                }
+
+                if (獲利 > 第三遠空單損失())
+                {
+                    double 損失 = 平第三遠多單();
+                    獲利 = 獲利 - 損失;
+                    有平空單 = true;
+                }
+
+                if (有平空單)
+                {
+                    OrderSend(TRADE_PAIR, OP_SELL, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                    加入賣單數量(0.01);
+                }
+            }
+
+            if (!是否有獲利多單 && !是否有獲利空單)
+            {
+                紀錄多空價格();
+            }
+        }
+
+        if (Bid紀錄 - Ask > 6)
+        {
+            平獲利空單();
+            紀錄多空價格();
+
+            double 獲利 = 計算本次空單獲利();
+
+            bool 有平多單 = false;
+
+            if (獲利 > 最遠多單損失())
+            {
+                double 損失 = 平最遠多單();
+                獲利 = 獲利 - 損失;
+                有平多單 = true;
+            }
+
+            if (獲利 > 第二遠多單損失())
+            {
+                double 損失 = 平第二遠多單();
+                獲利 = 獲利 - 損失;
+                有平多單 = true;
+            }
+
+            if (獲利 > 第三遠多單損失())
+            {
+                double 損失 = 平第三遠多單();
+                獲利 = 獲利 - 損失;
+                有平多單 = true;
+            }
+
+            if (有平多單)
+            {
+                OrderSend(TRADE_PAIR, OP_BUY, 0.01, Bid, 1, 0, 0, "", MAGIC_NUMBER, 0, clrNONE);
+                加入買單數量(0.01);
+            }
+        }
+    }
+}
+//+------------------------------------------------------------------+
